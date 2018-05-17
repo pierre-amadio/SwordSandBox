17391ff3aad091dba2250f81bb22173396601b90
Pierre Amadio <amadio.pierre@gmail.com>
2018-05-17 21:04:58 +0200

    not needed

--- qmlSwordReader/swordwrapper.cpp
+++ qmlSwordReader/swordwrapper.cpp
@@ -45,9 +45,9 @@ void swordWrapper::moduleNameChangedSlot(const QString &msg) {
 void swordWrapper::bookNameChangedSlot(const QString &curBook) {
     qDebug()<<"bookNameChangedSlot:"<<curBook;
     qDebug()<<"Chapter Max:"<<getChapterMax();
-    QQmlContext *rootContext = AppEngine->rootContext();
+    //QQmlContext *rootContext = AppEngine->rootContext();
     //qDebug()<<"max="<<getChapterMax();
-    rootContext->setContextProperty("maxChapter", QVariant::fromValue(getChapterMax()));
+    //rootContext->setContextProperty("maxChapter", QVariant::fromValue(getChapterMax()));
     QObject *rootObject = AppEngine->rootObjects().first();
     rootObject->setProperty("maxChapter", getChapterMax());
     maxChapterChanged(getChapterMax());
