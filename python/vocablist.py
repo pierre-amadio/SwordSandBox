209269fde78cb33420b1fb850f59f63d445d13db
Pierre Amadio <amadio.pierre@gmail.com>
2019-04-20 14:17:15 +0200

    cosmetic

--- python/vocablist.py
+++ python/vocablist.py
@@ -44,37 +44,21 @@ def getVerseMax(moduleName,bookName,chapterNbr):
 
 def show_available_modules():
     mgr = Sword.SWMgr()   
-    #mgr.prefixPath = swordDir
-    #mgr.configPath = "%s/mods.d" % swordDir
- 
     for m in mgr.getModules().values():
         print "%s -> %s "%(m.Name(),m.Description())
 
 def display_verse(key,moduleName,outputType=Sword.FMT_PLAIN):
     vk=Sword.VerseKey(key)
     markup=Sword.MarkupFilterMgr(outputType)
-    #markup=Sword.MarkupFilterMgr(Sword.FMT_HTML)
     markup.thisown=False
     mgr = Sword.SWMgr(markup)
-    #mgr.prefixPath = swordDir
-    #mgr.configPath = "%s/mods.d" % swordDir
     mod=mgr.getModule(moduleName)
     mod.setKey(vk)
     mgr.setGlobalOption("Hebrew Vowel Points", "On")
-    #mgr.setGlobalOption("Strong's Numbers","Off")
-    #mgr.setGlobalOption("Cross-references","Off")
-    #mgr.setGlobalOption("Morpheme Segmentation","Off")
-    #mgr.setGlobalOption("Morphological Tags","Off")
-    #mgr.setGlobalOption("Lemmas","Off")
-    #mgr.setGlobal:Option("Words of Christ in Red","Off")
-    #mgr.setGlobalOption("Textual Variants","Off")
-    #mgr.setGlobalOption("Word Javascript","Off")
-    #mgr.setGlobalOption("Transliterated Forms","Off")
 
     if not mod:
         print "No module"
         sys.exit()
-    
     return mod.renderText()
 
 bookStr="II Sam"
@@ -82,31 +66,18 @@ moduleStr="OSHB"
 strongModuleStr="StrongsHebrew"
 chapterInt=8
 
-#print getVerseMax("OSHB",bookStr,chapterInt)
-
 nameDic={}
 nameTotalCnt={}
 
 for verseNbr in range(1,1+getVerseMax(moduleStr,bookStr,chapterInt)):
-    #print verseNbr
     keySnt="%s %s:%s"%(bookStr,chapterInt,verseNbr)
-    #print keySnt
     rawVerse=display_verse(keySnt,moduleStr,Sword.FMT_HTML).getRawData()
-    #print rawVerse
     soup=BeautifulSoup(rawVerse)
     for w in soup.find_all(savlm=re.compile('strong')):
-       #print "w=",w
-       #print "savlm",w.get('savlm')
-       #print "get ", w.get_text().encode('utf-8').strip()
        strKeyGroup=re.match("strong:(.*)",w.get('savlm'))
        strKey=strKeyGroup.group(1)
-       #fullWord=w.get_text().encode('utf-8').strip()
        fullWord=w.get_text()
-       #print "strKey,",strKey
-       #print "############" 
        if strKey not in nameDic.keys():
-        #print strKey
-        #print fullWord.encode('utf-8').strip()
         nameTotalCnt[strKey]=1
         nameDic[strKey]=[]
         nameDic[strKey].append(fullWord)
@@ -124,19 +95,3 @@ for strK in sorted(nameTotalCnt, key=nameTotalCnt.__getitem__, reverse=True):
     print allVariants
     print " "
 
-'''
-rawVerse=display_verse("Ps 1:1",moduleStr,Sword.FMT_HTML).getRawData()
-soup=BeautifulSoup(rawVerse)
-for w in soup.find_all(savlm=re.compile('strong')):
-	print "w", w
-	print "savlm", w.get('savlm')
-	print "get text", w.get_text().encode('utf-8').strip()
-
-	strKeyGroup=re.match("strong:(.*)",w.get('savlm'))
-	strKey=strKeyGroup.group(1)
-	print strKey
-	#test=display_verse(strKey,strongModuleStr,Sword.FMT_PLAIN).getRawData()	
-	test=find_strong("03808",strongModuleStr,Sword.FMT_HTML).getRawData()	
-	print "test", test
-	print "#############"
-'''
