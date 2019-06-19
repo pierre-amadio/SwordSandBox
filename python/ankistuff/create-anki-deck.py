f2afd91353fd607e00949115a1696a14ace639ac
Pierre Amadio <amadio.pierre@gmail.com>
2019-06-19 08:01:49 +0200

    gpl v3

--- python/ankistuff/create-anki-deck.py
+++ python/ankistuff/create-anki-deck.py
@@ -1,5 +1,25 @@
 #!/home/melmoth/dev/ankiswordstuff/bin/python
 # -*- coding: utf-8 -*-
+"""
+    This file is part of create-anki-deck, a script that generate 
+    bible based vocavulary anki deck
+     
+    Copyright 2019 Pierre Amadio <pierre.amadio@laposte.net>
+
+    This script is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This script is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with This script.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
 import genanki
 import Sword
 import random
