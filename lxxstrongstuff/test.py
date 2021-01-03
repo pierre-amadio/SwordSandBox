5296fb2b06340d018d82bd2366525ebb37eb7b81
Pierre Amadio <amadio.pierre@gmail.com>
2021-01-03 18:26:17 +0100

    now, let s parse some xml

--- lxxstrongstuff/test.py
+++ lxxstrongstuff/test.py
@@ -1,12 +1,33 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # Trying to fill the gap in the LXX strong entry.
-# https://stackoverflow.com/questions/517923/what-is-the-best-way-to-remove-accents-normalize-in-a-python-unicode-string
 # https://docs.python.org/3/howto/unicode.html
 # https://github.com/openscriptures/strongs
 
+"""
+It is important to use accent because some sword entry differ only by accent.
+If not, the following entry will be confusing:
+
+απειμι αρα αυτου βατος ει εικω εις εκτος η μην ου ποτε που ρεω
+συνειμι ταυτα τις ω
+
+with accent we have still a duplicate problem with:
+ἄπειμι 548/549
+βάτος  942/943
+εἴκω   1502/1503
+μήν    3375/3376
+ῥέω    4482/4483
+σύνειμι 4895/4896
+ὦ       5599/5600
+
+"""
+
+
 import unicodedata
 import re
+import sys
+
+#From https://stackoverflow.com/questions/517923/what-is-the-best-way-to-remove-accents-normalize-in-a-python-unicode-string
 def strip_accents(s):
    return ''.join(c for c in unicodedata.normalize('NFD', s)
                      if unicodedata.category(c) != 'Mn')
@@ -15,21 +36,43 @@ def strip_accents(s):
 #print(test)
 #print(strip_accents(test))
 
-#StrongDicXML=/home/melmoth/dev/strongs/greek/StrongsGreekDictionaryXML_1.4/strongsgreek.xml
+#This comes from https://github.com/openscriptures/strongs
 StrongDic="/home/melmoth/dev/strongs/greek/strongs-greek-spellings.dic"
 
+#This comes from https://git.crosswire.org/cyrille/lxx
+lxxFile="/home/melmoth/dev/lxx/osis/lxx.osis.xml"
+
 def prepareDic(fileName):
-    print("Let s prepare dic")
+    """
+    Return a dictionnary wih    key=greek word wit accent
+                                value=strong number
+    """
+    out={}
     with open(fileName) as file_in:
             for line in file_in:
-                #print(line)
                 ma=re.match("^(\d+)\|(.+)$",line)
                 if ma:
                     #print("YES")
                     a=1
                     #print("'%s'+'%s'"%(ma.group(1),ma.group(2)))
+                    strongNbr=ma.group(1)
+                    strongWord=ma.group(2)
+                    strongNoAccent=strip_accents(strongWord)
+                    #print(strongNbr,strongWord,strongNoAccent)
+                    """
+                    if(strongWord in out.keys()):
+                        print("%s "%strongWord)
+                        sys.exit()
+                    """
+                    out[strongWord]=strongNbr
                 else:
-                    print("NO MATCH: '%s'"%line)
+                    a=1
+                    #print("NO MATCH: '%s'"%line)
+            return out
 
 
-dic=prepareDic(StrongDic)
+
+
+strongDic=prepareDic(StrongDic)
+#for i in dic:
+#    print(i,dic[i])
