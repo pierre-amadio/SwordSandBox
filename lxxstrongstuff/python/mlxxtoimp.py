f5b84d5a06d04c3d6aeae0c8d92c1b20c0626d8d
Pierre Amadio <amadio.pierre@gmail.com>
2021-01-15 14:01:42 +0100

    python betacode

---
+++ lxxstrongstuff/python/mlxxtoimp.py
@@ -0,0 +1,10 @@
+#!/home/melmoth/dev/ankiswordstuff/bin/python3
+
+"""
+reimplementation of https://crosswire.org/svn/sword-tools/trunk/modules/lxxm/src/lxxm/LXXMConv.java
+
+https://pypi.org/project/betacode/
+~/dev/ankiswordstuff/bin/python3.7  -m pip install  betacode
+"""
+
+
