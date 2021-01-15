5d0c52403a7eed03d4f7c008f1ac293270c99dd1
Pierre Amadio <amadio.pierre@gmail.com>
2021-01-15 14:20:52 +0100

    small lines and large lines

--- lxxstrongstuff/python/mlxxtoimp.py
+++ lxxstrongstuff/python/mlxxtoimp.py
@@ -5,6 +5,8 @@ reimplementation of https://crosswire.org/svn/sword-tools/trunk/modules/lxxm/src
 
 https://pypi.org/project/betacode/
 ~/dev/ankiswordstuff/bin/python3.7  -m pip install  betacode
+
+./mlxxtoimp.py ~/dev/lxx/scripts/lxxm-gen/lxxmorph/01.Gen.1.mlxx 
 """
 
 inputFile=sys.argv[1]
@@ -12,6 +14,18 @@ inputFile=sys.argv[1]
 #print(inputFile)
 
 with open(inputFile) as fp:
+    first=True
+    heading=False
+    headingTxt = ""
     for line in  fp:
-        print( line)
+        if(len(line)>0 and len(line)<36):
+            """
+                line smaller than 36 char, this is  probably the beginning of a verse or chapter.
+            """
+
+        else:
+            """
+                line larger than 36 char
+            """
+            print( line)
     fp.close()
