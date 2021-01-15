99bf4f46de93ee8b83b84556c33d7e356fa4be40
Pierre Amadio <amadio.pierre@gmail.com>
2021-01-15 14:11:13 +0100

    first test

--- lxxstrongstuff/python/mlxxtoimp.py
+++ lxxstrongstuff/python/mlxxtoimp.py
@@ -1,5 +1,5 @@
 #!/home/melmoth/dev/ankiswordstuff/bin/python3
-
+import sys
 """
 reimplementation of https://crosswire.org/svn/sword-tools/trunk/modules/lxxm/src/lxxm/LXXMConv.java
 
@@ -7,4 +7,11 @@ https://pypi.org/project/betacode/
 ~/dev/ankiswordstuff/bin/python3.7  -m pip install  betacode
 """
 
+inputFile=sys.argv[1]
 
+#print(inputFile)
+
+with open(inputFile) as fp:
+    for line in  fp:
+        print( line)
+    fp.close()
